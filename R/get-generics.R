8376ffa045239720035db51a6911161dcc183bdb
Krzysztof Sakrejda-Leavitt <sakrejda@cns.umass.edu>
2012-10-27 17:15:23 -0400

    Oops, was leaving out generics.

---
+++ R/get-generics.R
@@ -0,0 +1,30 @@
+
+
+setGeneric(name="get_N", 
+					 def=function(.Object) standardGeneric("get_N"), 
+					 valueClass = "numeric")
+setGeneric(name="get_K", 
+					 def=function(.Object) standardGeneric("get_K"), 
+					 valueClass = "numeric")
+setGeneric(name="get_recaptures", 
+					 def=function(.Object, id) standardGeneric("get_recaptures"),
+					 valueClass = "numeric")
+setGeneric(name="get_recaptures_matrix",
+					 def=function(.Object) standardGeneric("get_recaptures_matrix"),
+					 valueClass = "matrix")
+setGeneric(name="get_surveys",
+					 def=function(.Object) standardGeneric("get_surveys"),
+					 valueClass = "numeric")
+setGeneric(name="get_births",
+					 def=function(.Object) standardGeneric("get_births"),
+					 valueClass = "numeric")
+setGeneric(name="get_first_obs",
+					 def=function(.Object) standardGeneric("get_first_obs"),
+					 valueClass = "numeric")
+setGeneric(name="get_last_obs",
+					 def=function(.Object) standardGeneric("get_last_obs"),
+					 valueClass = "numeric")
+setGeneric(name="get_deaths",
+					 def=function(.Object) standardGeneric("get_deaths"),
+					 valueClass = "numeric")
+
---
+++ R/test-generics.R
@@ -0,0 +1,31 @@
+
+setGeneric(name="compare_K",
+					 def=function(.Object) standardGeneric("compare_K")
+					)
+setGeneric(name="compare_N",
+					 def=function(.Object) standardGeneric("compare_N")
+					)
+setGeneric(name="compare_surveys",
+					 def=function(.Object)
+						 standardGeneric("compare_surveys")
+					)
+setGeneric(name="compare_births",
+					 def=function(.Object)
+						 standardGeneric("compare_births")
+					)
+setGeneric(name="compare_deaths",
+					 def=function(.Object)
+						 standardGeneric("compare_deaths")
+					)
+setGeneric(name="compare_first_obs",
+					 def=function(.Object)
+						 standardGeneric("compare_first_obs")
+					)
+setGeneric(name="compare_last_obs",
+					 def=function(.Object)
+						 standardGeneric("compare_last_obs")
+					)
+setGeneric(name="compare_recaptures",
+					 def=function(.Object)
+						 standardGeneric("compare_recaptures")
+					)
